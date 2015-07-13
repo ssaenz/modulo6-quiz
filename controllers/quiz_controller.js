542c27750709d0a698b61617efe035bb3ede100c
ssaenz <ssaenz010@gmail.com>
2015-07-13 18:41:31 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+//GET /quizes/quiestion
+exports.question = function(req, res) {
+    res.render('quizes/question', {pregunta: 'Capital de Italia'});                                  
+};
+
+//GET /quizes/answer
+exports.answer = function(req, res) {
+    if (req.query.respuesta === 'Roma'){
+        res.render('quizes/answer', {respuesta: 'Correcto'});
+    } else {
+        res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+}
