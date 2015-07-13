f3bdbe46690e00ceaa2a8dee0d5af58abfd283e1
ssaenz <ssaenz010@gmail.com>
2015-07-13 22:49:48 +0200

    Nueva sección de créditos.

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET credits page. */
+router.get('/author', function(req, res) {
+  res.render('author', { });
+});
 module.exports = router;
